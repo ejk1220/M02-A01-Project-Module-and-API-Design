47a48ad7184641d695cc6944ed7f40b7cd673ddd
ejk1220 <ejkennedy137@gmail.com>
2021-12-09 12:55:44 -0500

    Added additonal map changes

--- Group9_WallyLandApp/src/main/java/LocationServices/UpdateMapUI.java
+++ Group9_WallyLandApp/src/main/java/LocationServices/UpdateMapUI.java
@@ -50,28 +50,28 @@ public class UpdateMapUI extends javax.swing.JDialog {
         jButton1.setText("Next");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                Nexttbutton2ActionPerformed(evt);
             }
         });
 
         jButton2.setText("Next");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                NextButton3ActionPerformed(evt);
             }
         });
 
         jButton3.setText("Next");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                Nextbutton1ActionPerformed(evt);
             }
         });
 
         jButton4.setText("Next");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                NextButton4ActionPerformed(evt);
             }
         });
 
@@ -116,21 +116,21 @@ public class UpdateMapUI extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void Nexttbutton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Nexttbutton2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_Nexttbutton2ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void NextButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NextButton3ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_NextButton3ActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void Nextbutton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Nextbutton1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_Nextbutton1ActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void NextButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NextButton4ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_NextButton4ActionPerformed
 
     /**
      * @param args the command line arguments
